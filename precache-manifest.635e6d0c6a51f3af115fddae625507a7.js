d3ae4169b13a96ba160db2d0d61e8586626e3140
Gokul <gokulmodi369@gmail.com>
2020-10-20 15:34:04 +0530

    Updates

---
+++ precache-manifest.635e6d0c6a51f3af115fddae625507a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7a86b258d2d88352809d796fd9a7b2",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "f975dd7bd69d945eb55b",
+    "url": "/react-website/static/css/main.7628e494.chunk.css"
+  },
+  {
+    "revision": "dee7bd362c4514e783f7",
+    "url": "/react-website/static/js/2.d0b86de4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-website/static/js/2.d0b86de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f975dd7bd69d945eb55b",
+    "url": "/react-website/static/js/main.d987b068.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  }
+]);
